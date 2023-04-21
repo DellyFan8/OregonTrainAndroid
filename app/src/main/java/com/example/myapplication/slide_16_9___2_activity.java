a6ce59a9d4ddbc296315ea30a8da11b988778e60
Aaron <aaronhesstablet@gmail.com>
2023-04-21 00:52:51 -0400

    First

---
+++ app/src/main/java/com/example/myapplication/slide_16_9___2_activity.java
@@ -0,0 +1,103 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		slide_16_9___2
+	 *	@date 		Friday 21st of April 2023 04:09:34 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.logging.Logger;
+
+	public class slide_16_9___2_activity extends Activity {
+
+	
+	private View _bg__slide_16_9___2_ek2;
+	private ImageView rectangle_1;
+	private View date_box;
+	private TextView date_message;
+	private TextView date_;
+	private TextView weather_message;
+	private TextView weather_;
+	private View locaton_box;
+	private TextView city_message;
+	private TextView location_;
+	private TextView distance_message;
+	private View locaton_box_ek1;
+	private TextView messages_go_here;
+	private View button5;
+	private TextView button_5;
+	private View button4;
+	private TextView button_4;
+	private View button3;
+	private TextView button_3;
+	private View button2;
+	private TextView button_2;
+	private View button1;
+	private TextView button_1;
+	private ImageView __6c361baa_5d76_4fdf_b970_3533203f6cc0_1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.slide_16_9___2);
+
+		
+		_bg__slide_16_9___2_ek2 = (View) findViewById(R.id._bg__slide_16_9___2_ek2);
+		rectangle_1 = (ImageView) findViewById(R.id.rectangle_1);
+		date_box = (View) findViewById(R.id.date_box);
+		date_message = (TextView) findViewById(R.id.date_message);
+		date_ = (TextView) findViewById(R.id.date_);
+		weather_message = (TextView) findViewById(R.id.weather_message);
+		weather_ = (TextView) findViewById(R.id.weather_);
+		locaton_box = (View) findViewById(R.id.locaton_box);
+		city_message = (TextView) findViewById(R.id.city_message);
+		location_ = (TextView) findViewById(R.id.location_);
+		distance_message = (TextView) findViewById(R.id.distance_message);
+		locaton_box_ek1 = (View) findViewById(R.id.locaton_box_ek1);
+		messages_go_here = (TextView) findViewById(R.id.messages_go_here);
+		button5 = (View) findViewById(R.id.button5);
+		button_5 = (TextView) findViewById(R.id.button_5);
+		button4 = (View) findViewById(R.id.button4);
+		button_4 = (TextView) findViewById(R.id.button_4);
+		button3 = (View) findViewById(R.id.button3);
+		button_3 = (TextView) findViewById(R.id.button_3);
+		button2 = (View) findViewById(R.id.button2);
+		button_2 = (TextView) findViewById(R.id.button_2);
+		button1 = (View) findViewById(R.id.button1);
+		button_1 = (TextView) findViewById(R.id.button_1);
+		__6c361baa_5d76_4fdf_b970_3533203f6cc0_1 = (ImageView) findViewById(R.id.__6c361baa_5d76_4fdf_b970_3533203f6cc0_1);
+	
+		
+		//custom code goes here
+
+		button5.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				setContentView(R.layout.slide_16_9___1);
+			}
+		});
+	
+	}
+}
+	
+	
